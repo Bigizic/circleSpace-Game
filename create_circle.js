550b65ede4965cf0465a291a88f4dceaf1b4e0bb
Bigizic <olalekanisaac75@gmail.com>
2023-08-23 20:31:08 +0100

    Add files via upload

---
+++ create_circle.js
@@ -0,0 +1,85 @@
+//creates the red circle and ouputs the score
+
+var x = 0;
+var y = 45;
+let btn = "";
+var counter = 0;
+var livesCounter = 2;
+var fillColor = "white";
+
+function circle(){
+let bun = document.getElementById("canvas");
+    btn = bun.getContext("2d");
+
+    btn.clearRect(0, 30, 800, 550);
+
+    btn.beginPath();
+    btn.arc(x, y, 6, 0, 2 * Math.PI);
+    btn.stroke();
+    btn.fillStyle="red";
+    btn.fill();
+
+    if (x < 0) {
+        /*window.location.replace('../index.html');*/
+        x = 819;
+    }
+
+requestAnimationFrame(circle);
+
+};
+circle();
+
+function myScore () {
+    let scoreTxt = document.getElementById("canvas");
+    btn = scoreTxt.getContext("2d");
+    const elementsId = ['up', 'left', 'down', 'right'];
+    const butt = elementsId.map(id => document.getElementById(id));
+    let clicked = false;
+    butt.forEach(butt => {
+        butt.addEventListener('click', () => {
+            if (!clicked)
+            {
+                clicked = true;
+                counter++;
+                drawScore();
+            }
+            else { clicked = false};
+        });
+    });
+    document.addEventListener("keydown", function(event) {
+        const arrowkeys = ['ArrowRight', 'ArrowLeft', 'ArrowUp', 'ArrowDown'];
+        if (arrowkeys.includes(event.key)) {
+            if (!clicked)
+            {
+                clicked = true;
+                counter++;
+                drawScore();
+            }
+            else { clicked = false};
+        }
+    });
+
+    function drawScore () {
+        btn.beginPath();
+        btn.clearRect(570, 10, 100, 100)
+        btn.fillStyle = `${fillColor}`;
+        btn.font = "25px monospace";
+        btn.fillText(`Score: ${counter}`, 500, 30,);
+        btn.fill();
+
+        btn.beginPath();
+        btn.fillStyle = `${fillColor}`;
+        btn.font = "20px monospace";
+        btn.fillText('score 50 or above!!', 580, 600,);
+        btn.fill();
+
+        btn.beginPath();
+        btn.fillStyle = `${fillColor}`;
+        btn.font = "25px monospace";
+        btn.fillText('Level: 1', 300, 30,);
+        btn.fill();
+
+    }
+    drawScore();
+};
+myScore();
---
+++ theme_togle.js
@@ -0,0 +1,87 @@
+function defaultTheme () {
+    const canvas = document.getElementById("canvas");
+    canvas.style.background = "url('./imgs/default_background.png')";
+    canvas.style.backgroundRepeat = "no-repeat";
+    canvas.style.backgroundSize = "101%";
+    fillColor = "white";
+};
+
+function thunder () {
+    const canvas = document.getElementById("canvas");
+    canvas.style.background = "url('./imgs/thunder.png')";
+    canvas.style.backgroundRepeat = "no-repeat";
+    canvas.style.backgroundSize = "101%";
+    fillColor = "white";
+};
+
+function whitenblack () {
+    const canvas = document.getElementById("canvas");
+    canvas.style.background = "url('./imgs/white_and_black.png')";
+    canvas.style.backgroundRepeat = "no-repeat";
+    canvas.style.backgroundSize = "101%";
+    fillColor = "white";
+};
+
+function blacknwhite () {
+    const canvas = document.getElementById("canvas");
+    canvas.style.background = "url('./imgs/black_and_white.png')";
+    canvas.style.backgroundRepeat = "no-repeat";
+    canvas.style.backgroundSize = "101%";
+    fillColor = "white";
+};
+
+function sketch () {
+    const canvas = document.getElementById("canvas");
+    canvas.style.background = "url('./imgs/sketch.png')";
+    canvas.style.backgroundRepeat = "no-repeat";
+    canvas.style.backgroundSize = "101%";
+    fillColor = "black";
+};
+
+function darkSketch () {
+    const canvas = document.getElementById("canvas");
+    canvas.style.background = "url('./imgs/dark_sketch.png')";
+    canvas.style.backgroundRepeat = "no-repeat";
+    canvas.style.backgroundSize = "101%";
+    fillColor = "black";
+};
+
+function fireTheme () {
+    const canvas = document.getElementById("canvas");
+    canvas.style.background = "url('./imgs/fire.png')";
+    canvas.style.backgroundRepeat = "no-repeat";
+    canvas.style.backgroundSize = "101%";
+    fillColor = "white";
+};
+
+function interTheme () {
+    const canvas = document.getElementById("canvas");
+    canvas.style.background = "url('./imgs/intersteller.png')";
+    canvas.style.backgroundRepeat = "no-repeat";
+    canvas.style.backgroundSize = "101%";
+    fillColor = "white";
+};
+
+function bubble_ani () {
+    const canvas = document.getElementById("canvas");
+    canvas.style.background = "url('./imgs/bubble_theme.gif')";
+    canvas.style.backgroundRepeat = "no-repeat";
+    canvas.style.backgroundSize = "103%";
+    fillColor = "white";
+};
+
+function rain_ani () {
+    const canvas = document.getElementById("canvas");
+    canvas.style.background = "url('./imgs/rain_and_thunder.gif')";
+    canvas.style.backgroundRepeat = "no-repeat";
+    canvas.style.backgroundSize = "98.5%";
+    fillColor = "white";
+};
+
+function snow_ani () {
+    const canvas = document.getElementById("canvas");
+    canvas.style.background = "url('snow.gif')";
+    canvas.style.backgroundRepeat = "no-repeat";
+    canvas.style.backgroundSize = "100%";
+    fillColor = "white";
+}
